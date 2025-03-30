088ea1e73bdd23cda96f8aa8975eeccf249fa4f2
PrudviTeja99 <prudvitejabandam987@gmail.com>
2025-03-30 08:52:47 +0530

    budget service as resource server

---
+++ BackEnd/budget-service/src/main/java/com/teja/budget_service/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.teja.budget_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.
+                authorizeHttpRequests(auth->auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2->oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
